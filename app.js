05b11ed5f0cceb4eb0c7355180f7cecfc9b9043a
Alex <alexprogressdeveloper@gmail.com>
2025-06-12 13:51:52 +0300

    routes

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res, next) => {
-  console.log("In the another middleware");
-  res.send("<h1>express</h1>");
+app.use(shopRoutes);
+app.use("/admin", adminRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page out</h1>");
 });
 
-app.use(shopRoutes);
-app.use(adminRoutes);
 app.listen(1000);
