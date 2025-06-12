457b3d6f8ebf22a60f2e1f8b09118613b2e494ca
Alex <alexprogressdeveloper@gmail.com>
2025-06-12 18:22:27 +0300

    styles public

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const path = require("path");
 const app = express();
 
 const adminRoutes = require("./routes/admin");
@@ -8,6 +8,7 @@ const shopRoutes = require("./routes/shop");
 const errorRoutes = require("./routes/error");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(shopRoutes);
 app.use("/admin", adminRoutes);
